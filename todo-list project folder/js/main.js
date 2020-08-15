858f6c9fa10c3a4cdebbd366073056338707912b
ibrahimgurhandev <ibrahimgurhan.dev@gmail.com>
2020-08-14 20:36:38 -0400

    final update for this week
    I completed the challenge: 5
    I feel good about my code: 4
    Finished the javascript logic, felt I
    did not use enough time for more css work, will come to this css and make it even better

--- todo-list project folder/js/main.js
+++ todo-list project folder/js/main.js
@@ -1,3 +1,6 @@
+// This is the code for the add button, it will take what ever is
+// in the input and create an li
+
 document.getElementById("addBtn").onclick = () => {
   var itemAdded = document.getElementById("input").value;
   var ulSection = document.getElementById("ulSection");
@@ -5,6 +8,8 @@ document.getElementById("addBtn").onclick = () => {
   ulSection.appendChild(liContainer);
   liContainer.innerHTML = itemAdded;
   totalLi();
+
+  //this makes any li clicked have a new class
   liContainer.addEventListener("click", () => {
     event.target.classList.toggle("completed");
     totalLi();
@@ -13,11 +18,11 @@ document.getElementById("addBtn").onclick = () => {
   var total = 0;
   function totalLi() {
     let totalItemsAdded = document.getElementById("NumItemsAdded");
-
+    // This will hold our counter for how many items there are.
+    //it will check if there are completed or not
     var total = 0;
     var numLi = document.querySelectorAll("li");
     for (let i = 0; i < numLi.length; i++) {
-      // if the list item (any of them) doesn't have a strike then...
       if (!numLi[i].classList.contains("completed")) {
         total++;
       }
@@ -25,12 +30,17 @@ document.getElementById("addBtn").onclick = () => {
     totalItemsAdded.innerHTML = total;
   }
 
+  //This simple function will just make everything inside
+  // our ul empty
   document.getElementById("clearAll").onclick = () => {
     ulSection.innerHTML = "";
     totalLi();
   };
 };
 
+// this is another simple function that will just
+// make any li that has completed class deleted when pressed on
+// the clear completed item button
 document.getElementById("clearItems").onclick = () => {
   var numLi = ulSection.querySelectorAll("li");
   var liContainer = document.createElement("li");
